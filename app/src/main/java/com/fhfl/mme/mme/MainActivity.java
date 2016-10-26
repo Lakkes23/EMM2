543bcd4d62b53be344fa433497ae06492c7637e9
janschill <schilllll@googlemail.com>
2016-10-26 12:53:27 +0200

    hello eem

--- app/src/main/java/com/fhfl/mme/mme/MainActivity.java
+++ app/src/main/java/com/fhfl/mme/mme/MainActivity.java
@@ -2,12 +2,17 @@ package com.fhfl.mme.mme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
     }
 }
